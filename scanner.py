e8521336bbf61dd22ea03ef39e4002d3dab8f149
Mauricio Sosa Giri <mauris@osomdigital.com>
2018-01-19 02:58:28 -0300

    first version
    Signed-off-by: Mauricio Sosa Giri <mauris@osomdigital.com>

---
+++ scanner.py
@@ -0,0 +1,69 @@
+import sys
+import socket
+import re
+from datetime import datetime
+
+
+while True:
+	initialIp = input("Enter the initial IP\n")
+	if not re.search("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", initialIp):
+		print("The IP is incorrect. Try again\n")
+	else:
+		break
+while True:
+	finalIp = input("Enter the final IP\n")
+	if not re.search("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$", finalIp):
+		print("The IP is incorrect. Try again\n")
+	else:
+		break
+while True:
+	initialPort = input("Enter the initial port\n")
+	if not re.search("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$", initialPort):
+		print("The port is incorrect. Try again\n")
+	else:
+		break
+while True:
+	finalPort = input("Enter the final port\n")
+	if not re.search("^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$", finalPort):
+		print("The port is incorrect. Try again\n")
+	else:
+		break
+
+
+
+print("Please wait, scanning...")
+initialIpSplitted = initialIp.split(".")
+finalIpSplitted = finalIp.split(".")
+incrementalNum0 = int(initialIpSplitted[0])
+incrementalNum1 = int(initialIpSplitted[1])
+incrementalNum2 = int(initialIpSplitted[2])
+incrementalNum3 = int(initialIpSplitted[3])
+finalIpSplitted[0] = int(finalIpSplitted[0])
+finalIpSplitted[1] = int(finalIpSplitted[1])
+finalIpSplitted[2] = int(finalIpSplitted[2])
+finalIpSplitted[3] = int(finalIpSplitted[3])
+
+
+timeStart = datetime.now()
+try:
+	for i in range(incrementalNum0, finalIpSplitted[0]+1):
+		for j in range(incrementalNum1, finalIpSplitted[1]+1):
+			for k in range(incrementalNum2, finalIpSplitted[2]+1):
+				for l in range(incrementalNum3, finalIpSplitted[3]+1):
+					ip = "%d.%d.%d.%d" % (i, j, k, l)
+					for port in range(int(initialPort),int(finalPort)):  
+						sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+						result = sock.connect_ex((str(i)+"."+str(j)+"."+str(k)+"."+str(l), port))
+						if result == 0:
+							sock.close()
+							print("IP: "+str(i)+"."+str(j)+"."+str(k)+"."+str(l)+" Port OPEN: "+str(port))
+							
+        				
+except KeyboardInterrupt:
+    print("You pressed Ctrl+C")
+    sys.exit()
+timeEnd = datetime.now()
+
+passedTime = timeEnd - timeStart
+print('Scanning Completed in:', passedTime)
+
